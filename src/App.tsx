9bcf4cc902d2822b4b9181f7ef30730f7477f08b
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-09-26 10:37:28 +0530

    connecting to metamask on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
+import { useContext, useEffect } from "react";
+
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
@@ -10,8 +12,20 @@ import Academia from "./pages/Academia";
 import Student from "./pages/Student";
 import Verifier from "./pages/Verifier";
 import UploadData from "./pages/UploadData";
+import { AccountContext, ContextObject } from "./context/Provider";
+import connectToMetamask from "./utils/connectMetamask";
+import getAccounts from "./utils/getAccounts";
 
 function App() {
+  const { acc, setAcc } = useContext<ContextObject>(AccountContext);
+  const getAcc = async () => {
+    acc && (await connectToMetamask());
+    const accounts: Array<string> = await getAccounts();
+    setAcc(accounts[0]);
+  };
+  useEffect(() => {
+    getAcc();
+  }, []);
   return (
     <>
       <NavBar />
