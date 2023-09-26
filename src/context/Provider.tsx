e0ca80a5ad6299c6754a1376830340c6ca6f4500
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-09-26 10:00:46 +0530

    created variables for owner and provider in context

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -2,20 +2,25 @@ import React, { createContext, useState } from "react";
 
 export type ContextObject = {
   acc: string;
+  isOwner: boolean;
   setAcc: React.Dispatch<React.SetStateAction<string>>;
+  setIsOwner: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 type ProviderProps = React.PropsWithChildren;
 
 export const AccountContext = createContext<ContextObject>({
   acc: "",
+  isOwner: false,
   setAcc: () => {},
+  setIsOwner: () => {},
 });
 
 const Provider = ({ children }: ProviderProps) => {
   const [acc, setAcc] = useState<string>("");
+  const [isOwner, setIsOwner] = useState<boolean>(false);
   return (
-    <AccountContext.Provider value={{ acc, setAcc }}>
+    <AccountContext.Provider value={{ acc, isOwner, setAcc, setIsOwner }}>
       {children}
     </AccountContext.Provider>
   );
