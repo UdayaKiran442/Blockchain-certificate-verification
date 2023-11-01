4dcd5d76eb39e418759b9eb73c319c780ffd15e9
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-11-01 15:40:57 +0530

    pinning files to ipfs

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,16 +3,18 @@ import React from "react";
 interface props {
   type: string;
   placeholder: string;
+  name: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-const InputField = ({ onChange, type, placeholder }: props) => {
+const InputField = ({ onChange, type, placeholder, ...props }: props) => {
   return (
     <input
       className="w-[30%] placeholder:p-2 placeholder:text-slate-400 placeholder:font-light rounded-md  border-primaryBlue border-2"
       type={type}
       placeholder={placeholder}
       onChange={onChange}
+      {...props}
     />
   );
 };
--- src/pages/UploadData.tsx
+++ src/pages/UploadData.tsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect, useState } from "react";
-import Papa from "papaparse";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import axios from "axios";
 
 import contractInstance from "../utils/contractInstance";
+import { pinata_api_key, pinata_secret_api_key } from "../utils/pinataKeys";
 
 import { AccountContext, ContextObject } from "../context/Provider";
 
@@ -11,34 +12,88 @@ import banner1 from "../assets/banner1.jpg";
 import banner2 from "../assets/banner2.jpeg";
 import banner3 from "../assets/banner3.jpg";
 
-interface CsvRow {
-  [key: string]: string | number | boolean | null;
-}
+import InputField from "../components/InputField";
 
 interface RegistrarObject {
   universityName: string;
   registrarAddress: string;
 }
 
+interface StudentObject {
+  studentName: string;
+  registrationNumber: string;
+  degree: string;
+  department: string;
+  batch: string;
+  cgpa: string;
+}
+
 const UploadData = () => {
-  const [csvData, setCsvData] = useState<CsvRow[]>([]);
   const [registrar, setRegistrar] = useState<RegistrarObject>({
     universityName: "",
     registrarAddress: "",
   });
+  const [studentData, setStudentData] = useState<StudentObject>({
+    studentName: "",
+    registrationNumber: "",
+    degree: "",
+    department: "",
+    batch: "",
+    cgpa: "",
+  });
+  const [photoHash, setPhotoHash] = useState<string>("");
+  const [certificateHash, setCertificateHash] = useState();
+  const [imageFile, setImageFile] = useState();
   const { acc, isValidRegistrar } = useContext<ContextObject>(AccountContext);
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onUpload = (e: any) => {
-    e.preventDefault();
-    const file = e.target.files[0];
-    Papa.parse(file, {
-      header: true,
-      complete: (result) => {
-        setCsvData(result.data as CsvRow[]);
-        console.log("Result", result);
-      },
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setStudentData({
+      ...studentData,
+      [name]: value,
     });
-    console.log("json:", csvData);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handlePhotoUpload = async (e: any) => {
+    e.preventDefault();
+    setImageFile(e.target.files[0]);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSubmit = async () => {
+    console.log("Button clicked");
+    console.log(studentData);
+    if (imageFile) {
+      const formData = new FormData();
+      formData.append("file", imageFile);
+      const resFile = await axios.post(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        formData,
+        {
+          headers: {
+            pinata_api_key,
+            pinata_secret_api_key,
+          },
+        }
+      );
+      console.log(resFile);
+      setPhotoHash(resFile.data.IpfsHash);
+    }
+    const resJson = await axios.post(
+      "https://api.pinata.cloud/pinning/pinJsonToIPFS",
+      studentData,
+      {
+        headers: {
+          pinata_api_key,
+          pinata_secret_api_key,
+        },
+      }
+    );
+    console.log(resJson);
+    setCertificateHash(resJson.data.IpfsHash);
+    console.log(photoHash, certificateHash);
   };
 
   useEffect(() => {
@@ -85,16 +140,55 @@ const UploadData = () => {
           single click
         </p>
       </div>
-      <div className="flex flex-col p-6 items-center">
-        <input
-          className="file:text-primaryBlue  file:bg-white  file:transition-colors file:py-1 file:px-5 file:cursor-pointer file:border-primaryBlue "
-          accept=".csv"
-          type="file"
-          onChange={onUpload}
+      {/* form */}
+      <div className="flex flex-col gap-4 items-center p-5">
+        <InputField
+          type="text"
+          placeholder="Student Name"
+          name="studentName"
+          onChange={handleChange}
         />
-        <p className="text-xs -ml-52 font-light">Only .csv files accepted</p>
-        <button className="text-secondaryWhite shadow-lg mt-5 bg-primaryBlue border border-blue-50 py-1 px-5">
-          Submit
+        <InputField
+          type="text"
+          placeholder="Registration Number"
+          name="registrationNumber"
+          onChange={handleChange}
+        />
+        <InputField
+          type="text"
+          placeholder="Degree"
+          name="degree"
+          onChange={handleChange}
+        />
+        <InputField
+          type="text"
+          placeholder="Department"
+          name="department"
+          onChange={handleChange}
+        />
+        <InputField
+          type="text"
+          placeholder="Batch"
+          name="batch"
+          onChange={handleChange}
+        />
+        <InputField
+          type="text"
+          placeholder="CGPA"
+          name="cgpa"
+          onChange={handleChange}
+        />
+        <input
+          onChange={handlePhotoUpload}
+          type="file"
+          className="file:text-primaryBlue  file:bg-white  file:transition-colors file:py-1 file:px-5 file:cursor-pointer file:border-primaryBlue "
+        />
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="bg-primaryBlue text-secondaryWhite px-8 py-2"
+        >
+          Upload Data
         </button>
       </div>
     </div>
